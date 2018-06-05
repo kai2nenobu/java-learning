d15e26074bb909e681e6a505d29faa1310e53c3a
Tsunenobu Kai <kai2nenobu@gmail.com>
2018-06-05 11:45:03 +0900

    Pattern の UNICODE_CHARACTER_CLASS フラグの挙動を確認する学習テスト

---
+++ lang/src/test/java/learning/java/util/regex/PatternLearning.java
@@ -0,0 +1,32 @@
+package learning.java.util.regex;
+
+import static org.junit.Assert.*;
+
+import java.util.regex.Pattern;
+
+import org.junit.Test;
+
+public class PatternLearning {
+    Pattern printable = Pattern.compile("[\\p{Print}]*");
+    Pattern unicodePrintable = Pattern.compile("[\\p{Print}]*", Pattern.UNICODE_CHARACTER_CLASS);
+
+    @Test
+    public void aはprintableである() {
+        assertTrue(printable.matcher("a").matches());
+    }
+
+    @Test
+    public void あはprintableではない() {
+        assertFalse(printable.matcher("あ").matches());
+    }
+
+    @Test
+    public void aはUnicodePrinatbleである() {
+        assertTrue(unicodePrintable.matcher("a").matches());
+    }
+
+    @Test
+    public void あはUnicodePrinatbleである() {
+        assertTrue(unicodePrintable.matcher("あ").matches());
+    }
+}
